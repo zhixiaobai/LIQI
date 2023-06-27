9b28063d0706a43bc0be2f2f5fa1a75cba994b65
25350 <2535002576@qq.com>
2023-06-27 10:15:37 +0800

    feat: 新增DMLHandler处理器、DMLExecutor执行器 - 新增DMLHandler处理器用于处理除Select以外的DML操作 - 新增DMLExecutor执行器用于执行除Select以外的DML操作

---
+++ src/main/java/com/zxb/liqi/core/executor/DMLExecutor.java
@@ -0,0 +1,92 @@
+package com.zxb.liqi.core.executor;
+
+import com.zxb.liqi.interfaces.BaseExecutor;
+import com.zxb.liqi.interfaces.BaseHandler;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * @author Mr.M
+ * @date 2023/6/27
+ * @Description
+ */
+public class DMLExecutor implements BaseExecutor {
+    private final BaseHandler handler;
+    private final Class<?> entityClass;
+    private final PreparedStatement preparedStatement;
+    private final Connection connection;
+
+    public DMLExecutor(BaseHandler handler, Class<?> entityClass, Connection connection, PreparedStatement preparedStatement) {
+        this.entityClass = entityClass;
+        this.handler = handler;
+        this.connection = connection;
+        this.preparedStatement = preparedStatement;
+    }
+
+    private int[] mergeArrays(int[] target, int[] source) {
+        return IntStream.concat(Arrays.stream(target), Arrays.stream(source))
+                .toArray();
+    }
+
+    private int singleSqlExec() throws SQLException {
+        int parameterIndex = 1;
+        for (Object obj : this.handler.getRealDataMap().values()) {
+            this.preparedStatement.setObject(parameterIndex, obj);
+            parameterIndex ++;
+        }
+        return this.preparedStatement.executeUpdate();
+    }
+
+    private int multipleSqlExec() throws SQLException {
+        this.connection.setAutoCommit(false);
+        int[] mysqlAffectedRows = new int[0];
+        for (int i = 0; i < this.handler.getSqlCount(); i++) {
+            int parameterIndex = 1;
+            for (Object obj : this.handler.getRealDataMap().values()) {
+                if (obj instanceof List) {
+                    this.preparedStatement.setObject(parameterIndex, ((List<?>) obj).get(i));
+                } else {
+                    this.preparedStatement.setObject(parameterIndex, obj);
+                }
+                parameterIndex ++;
+            }
+            this.preparedStatement.addBatch();
+            if (i % this.handler.getBaseParser().getBatchSize() == 0) {
+                mysqlAffectedRows = mergeArrays(mysqlAffectedRows, this.preparedStatement.executeBatch());
+                this.connection.commit();
+                this.preparedStatement.clearBatch();
+            }
+        }
+        mysqlAffectedRows = mergeArrays(mysqlAffectedRows, this.preparedStatement.executeBatch());
+        this.connection.commit();
+        int mysqlAffectedRow = 0;
+        for (int row: mysqlAffectedRows) {
+            if (row >= 0) {
+                mysqlAffectedRow = mysqlAffectedRow + row;
+            }
+            if (row == -2) {
+                mysqlAffectedRow = mysqlAffectedRow + 1;
+            }
+        }
+        return mysqlAffectedRow;
+    }
+
+    @Override
+    public Object executor() {
+        try {
+            if (this.handler.getSqlCount() == 1) {
+                return this.singleSqlExec();
+            } else {
+                return this.multipleSqlExec();
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            throw new RuntimeException(exception);
+        }
+    }
+}
---
+++ src/main/java/com/zxb/liqi/core/handler/DMLHandler.java
@@ -0,0 +1,133 @@
+package com.zxb.liqi.core.handler;
+
+import com.zxb.liqi.exception.GetBeanInfoErrorException;
+import com.zxb.liqi.exception.NoFoundEntityFieldException;
+import com.zxb.liqi.exception.ObjGetMethodInvokeErrorException;
+import com.zxb.liqi.interfaces.BaseHandler;
+import com.zxb.liqi.interfaces.BaseParser;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * @author Mr.M
+ * @date 2023/6/27
+ * @Description
+ */
+public class DMLHandler implements BaseHandler {
+
+    private final BaseParser parser;
+    private final static String SPLIT_SUFFIX = "\\.";
+    private final static String REGEX = "#\\{( *\\b\\w+.?\\w+\\b *)}";
+    private Map<String, Object> realParamData;
+    private String prepareSql;
+    private int sqlCount = 1;
+
+    public DMLHandler(BaseParser parser) {
+        this.parser = parser;
+        this.handle();
+    }
+
+    private String preparedStatementSql(String sql) {
+        Map<String, Object> paramMap = this.parser.getParamMap();
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(sql);
+
+        realParamData = new LinkedHashMap<>();
+
+        while (matcher.find()) {
+            String matcherStr = matcher.group(1).trim();
+
+            String paramKey = null;
+            if (matcherStr.contains(".")) {
+                paramKey = matcherStr.split(SPLIT_SUFFIX)[1];
+                matcherStr = matcherStr.split(SPLIT_SUFFIX)[0];
+            }
+            if (paramMap.containsKey(matcherStr)) {
+                String paramName = Objects.isNull(paramKey) ? matcherStr : matcherStr + "." + paramKey;
+                Object obj;
+                if (Objects.isNull(paramKey)) {
+                    obj = paramMap.get(matcherStr);
+                    if (obj instanceof List && !((List<?>) obj).isEmpty()) {
+                        this.sqlCount = Math.max(this.sqlCount, ((List<?>) obj).size());
+                    }
+                } else {
+                    obj = paramMap.get(matcherStr);
+                    if (obj instanceof List && !((List<?>) obj).isEmpty()) {
+                        final String finalParamKey = paramKey;
+                        obj = ((List<?>) obj).stream().map(x -> getParamValue(x, finalParamKey))
+                                .collect(Collectors.toList());
+                        this.sqlCount = Math.max(this.sqlCount, ((List<?>) obj).size());
+                    } else {
+                        obj = getParamValue(obj, paramKey);
+                    }
+                }
+                if (realParamData.containsKey(paramName)) {
+                    throw new RuntimeException("不能存在相同paramKey");
+                }
+                sql = sql.replace("#{" + paramName + "}", "?");
+                realParamData.put(paramName, obj);
+            }
+        }
+        return sql;
+    }
+
+    private Object getParamValue(Object obj, String objPropertyName) {
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+            PropertyDescriptor pd = null;
+            for (PropertyDescriptor propertyDescriptor : beanInfo.getPropertyDescriptors()) {
+                if (propertyDescriptor.getName().equals(objPropertyName)) {
+                    pd = propertyDescriptor;
+                    break;
+                }
+            }
+            // 获取 objPropertyName 对应属性值
+            if (Objects.nonNull(pd)) {
+                return pd.getReadMethod().invoke(obj);
+            }
+            throw new NoFoundEntityFieldException("Not found in the current entity class " + objPropertyName + " field");
+        } catch (IntrospectionException e) {
+            e.printStackTrace();
+            throw new GetBeanInfoErrorException("Get the current " + obj.getClass() + " class beanInfo object failed");
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+            throw new ObjGetMethodInvokeErrorException("The current object " + objPropertyName + " property getter method invoke failure");
+        }
+    }
+
+    @Override
+    public void handle() {
+        this.prepareSql = this.preparedStatementSql(this.parser.getParseSql());
+    }
+
+    @Override
+    public Map<String, Object> getRealDataMap() {
+        return this.realParamData;
+    }
+
+    @Override
+    public String getPrepareSql() {
+        return this.prepareSql;
+    }
+
+    @Override
+    public BaseParser getBaseParser() {
+        return this.parser;
+    }
+
+    @Override
+    public int getSqlCount() {
+        return this.sqlCount;
+    }
+}

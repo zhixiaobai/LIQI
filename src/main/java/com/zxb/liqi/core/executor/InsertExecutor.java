a001b220fb7abe5d0d741cfb9f303ea45c0bfcbf
25350 <2535002576@qq.com>
2023-06-27 09:54:18 +0800

    update: 更新InsertExecutor执行器 - 新增singleSqlExec - 新增multipleSqlExec - 修改executor业务逻辑

--- src/main/java/com/zxb/liqi/core/executor/InsertExecutor.java
+++ src/main/java/com/zxb/liqi/core/executor/InsertExecutor.java
@@ -5,6 +5,7 @@ import com.zxb.liqi.interfaces.BaseHandler;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -32,40 +33,57 @@ public class InsertExecutor implements BaseExecutor {
                 .toArray();
     }
 
+    private int singleSqlExec() throws SQLException {
+        int parameterIndex = 1;
+        for (Object obj : this.handler.getRealDataMap().values()) {
+            this.preparedStatement.setObject(parameterIndex, obj);
+            parameterIndex ++;
+        }
+        return this.preparedStatement.executeUpdate();
+    }
+
+    private int multipleSqlExec() throws SQLException {
+        this.connection.setAutoCommit(false);
+        int[] mysqlAffectedRows = new int[0];
+        for (int i = 0; i < this.handler.getSqlCount(); i++) {
+            int parameterIndex = 1;
+            for (Object obj : this.handler.getRealDataMap().values()) {
+                if (obj instanceof List) {
+                    this.preparedStatement.setObject(parameterIndex, ((List<?>) obj).get(i));
+                } else {
+                    this.preparedStatement.setObject(parameterIndex, obj);
+                }
+                parameterIndex ++;
+            }
+            this.preparedStatement.addBatch();
+            if (i % this.handler.getBaseParser().getBatchSize() == 0) {
+                mysqlAffectedRows = mergeArrays(mysqlAffectedRows, this.preparedStatement.executeBatch());
+                this.connection.commit();
+                this.preparedStatement.clearBatch();
+            }
+        }
+        mysqlAffectedRows = mergeArrays(mysqlAffectedRows, this.preparedStatement.executeBatch());
+        this.connection.commit();
+        int mysqlAffectedRow = 0;
+        for (int row: mysqlAffectedRows) {
+            if (row >= 0) {
+                mysqlAffectedRow = mysqlAffectedRow + row;
+            }
+            if (row == -2) {
+                mysqlAffectedRow = mysqlAffectedRow + 1;
+            }
+        }
+        return mysqlAffectedRow;
+    }
+
     @Override
     public Object executor() {
         try {
-            this.connection.setAutoCommit(false);
-            int[] mysqlAffectedRows = new int[0];
-            for (int i = 0; i < this.handler.getSqlCount(); i++) {
-                int parameterIndex = 1;
-                for (Object obj : this.handler.getRealDataMap().values()) {
-                    if (obj instanceof List) {
-                        preparedStatement.setObject(parameterIndex, ((List<?>) obj).get(i));
-                    } else {
-                        preparedStatement.setObject(parameterIndex, obj);
-                    }
-                    parameterIndex ++;
-                }
-                preparedStatement.addBatch();
-                if (i % this.handler.getBaseParser().getBatchSize() == 0) {
-                    mysqlAffectedRows = mergeArrays(mysqlAffectedRows, preparedStatement.executeBatch());
-                    this.connection.commit();
-                    preparedStatement.clearBatch();
-                }
+            if (this.handler.getSqlCount() == 1) {
+                return this.singleSqlExec();
+            } else {
+                return this.multipleSqlExec();
             }
-            mysqlAffectedRows = mergeArrays(mysqlAffectedRows, preparedStatement.executeBatch());
-            this.connection.commit();
-            int mysqlAffectedRow = 0;
-            for (int row: mysqlAffectedRows) {
-                if (row >= 0) {
-                    mysqlAffectedRow = mysqlAffectedRow + row;
-                }
-                if (row == -2) {
-                    mysqlAffectedRow = mysqlAffectedRow + 1;
-                }
-            }
-            return mysqlAffectedRow;
         } catch (Exception exception) {
             exception.printStackTrace();
             throw new RuntimeException(exception);
